aa2a480f8ff6cb564c802d00d22d24a419869af2
Fredrik <fredrikbach1@gmail.com>
2021-03-24 17:22:01 +0100

    add font

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ body {
     margin: 0%;
 }
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Tahoma, Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
