1c86295dfb5ee1bd715f7531eaf2a9b5b562bab8
Simon Riemertzon <simon.riemertzon@gmail.com>
2021-04-03 13:40:47 +0200

    Show temperatures in graph

--- src/components/TempGraph.vue
+++ src/components/TempGraph.vue
@@ -1,5 +1,16 @@
 <template>
-  <div><p>Hejhej</p></div>
+  <div>
+    <div class="dayselector">Måndag Tisdag Onsdag...</div>
+    <div class="city">Göteborg</div>
+    <div class="chart">
+      <apexchart
+        type="bar"
+        height="400"
+        :options="chartOptions"
+        :series="series"
+      ></apexchart>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
@@ -7,12 +18,41 @@
 <script>
 //import ApexCharts from "apexcharts";
 
+import { format } from "date-fns";
 export default {
   name: "TempGraph",
   props: {},
   data() {
     return {
-      tempForcastDataList: [],
+      tempListValues: [],
+      tempListTimes: [],
+
+      series: [],
+      chartOptions: {
+        chart: {
+          type: "bar",
+          height: 400,
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: "100%",
+            endingShape: "rounded",
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ["transparent"],
+        },
+
+        xaxis: {
+          categories: [],
+        },
+      },
     };
   },
   methods: {
@@ -23,14 +63,34 @@ export default {
       );
       const json = await response.json();
 
-      for (const timeSeries of json.timeSeries) {
-        let tempForecastData = {
-          validTime: timeSeries.validTime,
-          temperature: timeSeries.parameters[0].values,
-        };
-        this.tempForcastDataList.push(tempForecastData);
+      for (const hourlyData of json.timeSeries) {
+        const temp = this.findTemperature(hourlyData.parameters);
+        this.tempListValues.push(temp);
+        this.tempListTimes.push(hourlyData.validTime);
       }
-      console.log("Here is the list:", this.tempForcastDataList);
+      this.tempListValues.length = 24;
+      this.tempListTimes.length = 24;
+      this.displayGraph();
+    },
+    displayGraph() {
+      this.series = [
+        {
+          name: "Temperaturer",
+          data: this.tempListValues,
+        },
+      ];
+      this.chartOptions.xaxis.categories.length = 0;
+      for (let val of this.tempListTimes) {
+        let formattedDate = format(new Date(val), "HH");
+        this.chartOptions.xaxis.categories.push(formattedDate);
+      }
+    },
+    findTemperature(parameters) {
+      for (const param of parameters) {
+        if (param.name === "t") {
+          return param.values[0];
+        }
+      }
     },
   },
 
