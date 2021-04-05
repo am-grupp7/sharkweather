8b6246dc97ec13f9c8ee59f74ada802dce8dd65f
Fredrik <fredrikbach1@gmail.com>
2021-04-05 22:01:56 +0200

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <div id="nav">
       <router-link to="/">Start</router-link>
       <router-link to="/TopTen">Topp 10-lista</router-link>
-      <router-link to="/">Soltimmar</router-link>
       <router-link to="/TempForecast">Temperaturprognos</router-link>
       <router-link to="/PrecipForecast">Nederbördsprognos</router-link>
       <router-link to="/About">Om oss</router-link>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,43 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
+        <!--<img alt="Vue logo" src="../assets/logo.png" />-->
+        <div class="wilkommen">
+            <h1>Välkommen!</h1>
+            <p>
+                För att snabbare komma igång ber vi dig att välja din <br />
+                plats, den kommer vara default för informationen som <br />
+                visas, men du kan även ändra den senare.
+            </p>
+        </div>
+        <div class="lower-row">
+            <div class="inner">
+                <h3>Dagens topp 3</h3>
+                <div class="top-tre"></div>
+            </div>
+            <div class="inner">
+                <div class="find-location">
+                    <div class="serchbar">
+                        <input
+                            type="text"
+                            name="serch-location"
+                            placeholder="Sök efter en plats"
+                        />
+                    </div>
+                    <p>eller...</p>
+                    <div class="your-location">
+                        <input
+                            type="button"
+                            value="Använd din nuvarande plats"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div class="inner">
+                <div class="umbrella">
+                    <img alt="Vue logo" src="../assets/icons/Regn.svg" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,3 +48,21 @@ export default {
     name: 'Home',
 }
 </script>
+
+<style scoped>
+.home {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 30% 40% 30%;
+}
+.wilkommen {
+    background-color: #daeafb;
+}
+.find-location {
+    background-color: #daeafb;
+}
+.lower-row {
+    display: flex;
+    flex-direction: row;
+}
+</style>
